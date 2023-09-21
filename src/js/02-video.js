2181e53d7ef9b41990efcf4c0b49e7eb8cb7fc6f
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-21 13:48:34 +0300

    start of 2 task hw-8, install lodash.throttle and vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.getCurrentTime().then(function(data) {
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
