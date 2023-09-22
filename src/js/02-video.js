151375298e5b4fea3b60ce4d19ab0956c1875e0b
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-22 16:09:08 +0300

    task 2 and 3 without libraries

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,16 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
-const video = document.querySelector('#vimeo-player');
 const keyToStorage = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
 
+const player = new Player(iframe);
 
+function getTime (currentTime) {
+    const time = currentTime.seconds;
+    localStorage.setItem(keyToStorage, time);
+}
+
+player.on('timeupdate', getTime);
+
+player.setCurrentTime(localStorage.getItem(keyToStorage) || 0);
